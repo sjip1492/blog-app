a00a168c6bb11c910f4209e62d9e5dc10626b4db
sarah <sarahjadepratt@gmail.com>
2020-10-01 12:08:13 -0700

    second implementation complete with event bus and query service

---
+++ event-bus/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
+
+    res.send({status: 'OK'});
+});
+
+// listen on event-bus port
+app.listen(4006, () => {
+    console.log('Listening on port 4006');
+})
