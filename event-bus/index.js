621f471cf90318fa493ab3958b4c83cf4487d223
sarah <sarahjadepratt@gmail.com>
2020-10-02 14:07:57 -0700

    adding comment moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://localhost:4001/events', event); //posts
+    axios.post('http://localhost:4002/events', event); // comments
+    axios.post('http://localhost:4003/events', event); // query
+    axios.post('http://localhost:4004/events', event); // moderation
 
     res.send({status: 'OK'});
 });
---
+++ moderation/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        // validating the moderation status of the comment's content
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4006/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+
+    res.send({});
+});
+
+app.listen(4004, () => {
+    console.log('Listening on 4004');
+});
